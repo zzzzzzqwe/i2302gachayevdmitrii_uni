10a7378b23192e213b8651228abb566739cbef10
zzzzzzqwe <dima.gachayev@gmail.com>
2023-10-02 20:16:40 +0300

    Created

---
+++ Lab2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int number = 72;
+    i = 69 + 72;
+    int array[3] int *pointeri = &i;
+    int number = *pointeri;
+    *pointeri = 15;
+
+
+
+}
