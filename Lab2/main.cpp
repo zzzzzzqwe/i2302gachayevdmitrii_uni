85282b904f5f47d938a44f7aff2c399b51d6ec0c
zzzzzzqwe <dima.gachayev@gmail.com>
2023-10-02 23:10:25 +0300

    1

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 int main()
 {
-    int i;
-    int number = 72;
+    int i = 69;
+    int number;
+    number = 72;
     i = 69 + 72;
-    int array[3] int *pointeri = &i;
-    int number = *pointeri;
-    *pointeri = 15;
+    int array[3];
+    int *iPointer = &i;
+    number = *iPointer;
+    *iPointer = 15;
+    iPointer = /*&*/array[0];
+    iPointer += 2;
+    array[0] = 5;
+    *iPointer = 6;
 
-
-
+    return 0;
 }
